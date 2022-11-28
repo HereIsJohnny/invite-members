3413ed12b991f3442e1a55b50c671fc391db3f7e
Maciej Kowalski <maciej.kowalski.mk@gmail.com>
2022-11-28 20:23:02 +0100

    initial setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+// 1. Import the extendTheme function
+import { extendTheme } from '@chakra-ui/react';
+import { m } from 'framer-motion';
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    grey: {
+      200: '#47475E',
+      800: '#E7ECF0',
+    },
+  },
+};
+
+const theme = extendTheme({
+  colors,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
